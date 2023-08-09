93e26eb76b07c07c6b2a2a2b50c18dc1414724b2
Noughel12 <122110200+Noughel12@users.noreply.github.com>
2023-08-09 11:15:26 +0100

    Update Scan_ports.py

--- Scan_ports.py
+++ Scan_ports.py
@@ -1,5 +1,10 @@
 import socket
 import argparse
+from ftplib import FTP
+import telnetlib3
+
+from smb.SMBConnection import SMBConnection
+
 
 def scan_ports(ip_address, ports):
     open_ports = []
@@ -76,6 +81,115 @@ def save_results_to_file(results, filename):
                 file.write(f"{str(port).ljust(6)} open  {service_name}\n")
             file.write("\n")
 
+
+def connect_ftp(ip_address, port):
+    try:
+        with FTP() as ftp:
+            ftp.connect(ip_address, port)
+            ftp.login()
+            
+            server_info = ftp.getwelcome()  # Get server information
+            print(f"Server Information for FTP server ({ip_address}:{port}):")
+            print(server_info)
+            
+            # Get the list of files in the FTP root directory
+            file_list = ftp.nlst()
+            print(f"\nList of files in FTP server ({ip_address}:{port}):")
+            for file in file_list:
+                print(file)
+            
+            # Download each file from the server
+            for remote_filename in file_list:
+                local_filename = "downloaded_" + remote_filename  # Adjust the local file name if needed
+                
+                try:
+                    with open(local_filename, "wb") as local_file:
+                        ftp.retrbinary("RETR " + remote_filename, local_file.write)
+                        print(f"\nFile '{remote_filename}' downloaded and saved as '{local_filename}'")
+                except Exception as download_error:
+                    print(f"\nError downloading file '{remote_filename}': {download_error}")
+            
+            ftp.quit()
+            return True
+    except Exception as e:
+        return False
+
+
+
+def connect_telnet(ip_address, port):
+    try:
+        conn = telnetlib3.open_connection(ip_address, port)
+        conn.wait_for_close()
+        return True
+    except Exception as e:
+        return False
+#def connect_telnet(ip_address, port):
+    try:
+        conn = telnetlib3.open_connection(ip_address, port)
+        print(f"Telnet connection succeeded for {ip_address}:{port}")
+        
+        # Lire la bannière du serveur
+        server_banner = conn.read_until(b"\n").decode("utf-8")
+        print("Server Banner:")
+        print(server_banner)
+        
+        # Attendre l'invite de commande
+        conn.wait_for(b"Command Prompt:")
+        print("Connected to Command Prompt")
+        
+        # Envoyer la commande 'help' pour obtenir les commandes et options disponibles
+        conn.write("help\r\n")
+        commands_info = conn.read_until(b"Command Prompt:").decode("utf-8")
+        print("Commands and Options:")
+        print(commands_info)
+        
+        # Envoyer une commande spécifique (par exemple, 'system') pour obtenir des informations sur l'état du système
+        conn.write("system\r\n")
+        system_info = conn.read_until(b"Command Prompt:").decode("utf-8")
+        print("System Status:")
+        print(system_info)
+
+        # Envoyer une commande personnalisée (par exemple, 'custom_command') et afficher la réponse
+        conn.write("custom_command\r\n")
+        custom_response = conn.read_until(b"Command Prompt:").decode("utf-8")
+        print("Custom Command Response:")
+        print(custom_response)
+        
+        # Fermer la connexion
+        conn.close()
+        return True
+    except Exception as e:
+        print(f"Telnet connection failed for {ip_address}:{port}: {str(e)}")
+        return False
+
+
+def connect_smb(ip_address, port):
+    try:
+        # Créer une instance de SMBConnection
+        smb_connection = SMBConnection('username', 'password', 'client_name', 'server_name', use_ntlm_v2=True)
+
+        # Se connecter au serveur SMB
+        if smb_connection.connect(ip_address, port):
+            print(f"SMB connection succeeded for {ip_address}:{port}")
+
+            # Exemple : Lister les fichiers dans un répertoire partagé
+            share_name = 'ShareName'
+            directory = '\\'
+            file_list = smb_connection.listPath(share_name, directory)
+
+            print(f"List of files in SMB share {share_name} on {ip_address}:{port}:")
+            for file_info in file_list:
+                print(file_info.filename)
+ # Se déconnecter du serveur SMB
+            smb_connection.close()
+            return True
+        else:
+            print(f"SMB connection failed for {ip_address}:{port}")
+            return False
+    except Exception as e:
+        print(f"Error during SMB connection for {ip_address}:{port}: {str(e)}")
+        return False
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Scan des ports ouverts sur une liste d'adresses IP.")
     parser.add_argument("ip_file", help="Fichier contenant les adresses IP à scanner.")
@@ -97,6 +211,25 @@ if __name__ == "__main__":
             results[ip_address] = open_ports
             print_scan_results(ip_address, open_ports)
 
+
+            # Tester les ports FTP et Telnet
+            for port in open_ports:
+                if port == 21:
+                    if connect_ftp(ip_address, port):
+                        print(f"FTP connection succeeded for {ip_address}:{port}")
+                    else:
+                        print(f"FTP connection failed for {ip_address}:{port}")
+                elif port == 23:
+                    if connect_telnet(ip_address, port):
+                        print(f"Telnet connection succeeded for {ip_address}:{port}")
+                    else:
+                        print(f"Telnet connection failed for {ip_address}:{port}")
+                elif port == 445:
+                    if connect_smb(ip_address, port):
+                        print(f"SMB connection succeeded for {ip_address}:{port}")
+                    else:
+                        print(f"SMB connection failed for {ip_address}:{port}")
+        
         # Déterminer le nom du fichier de sortie
         if args.output:
             output_filename = args.output
